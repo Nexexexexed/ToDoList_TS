db46450cf2db25aace659d5309b044a339f17a8a
Nick <144842765+Nexexexexed@users.noreply.github.com>
2025-02-24 18:09:10 +0300

    create Store,Reducer,Actions

---
+++ ToDoList/src/redux/todoSlice.ts
@@ -0,0 +1,41 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+interface TodoState {
+  todo: Todo[];
+}
+
+const initialState: TodoState = {
+  todo: [],
+};
+
+const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action: PayloadAction<string>) => {
+      state.todo.push({
+        id: Date.now(),
+        text: action.payload,
+        completed: false,
+      });
+    },
+    toggleTodo: (state, action: PayloadAction<number>) => {
+      const todo = state.todo.find((t) => t.id === action.payload);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
+    },
+    removeTodo: (state, action: PayloadAction<number>) => {
+      state.todo.filter((t) => t.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, toggleTodo, removeTodo } = todoSlice.actions;
+export default todoSlice.reducer;
